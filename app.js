dd4d6dff964c1b048010dff25d79d5865aff16c2
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-06-11 11:57:06 +0100

    Automatic reloading (nodemon), logging(morgan) and error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//if we reach this line and make it past the above 2 middlewares - 
+//no routes in the previous two middle wares were able to handle the request
+//so we can handle errors by catching all requests that make it past the above middlewares
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
